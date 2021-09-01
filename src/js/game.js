3168483ca55d654cf578831a6234845de3e60891
Paige Tiedeman <paigetiedeman@gmail.com>
2021-09-01 13:46:26 -0700

    test for player turn passed

--- src/js/game.js
+++ src/js/game.js
@@ -1,5 +1,10 @@
 import Character from "./character.js";
 
 export default class Game {
-  
+  constructor (playerOne, playerTwo) {
+    this.playerOne = playerOne;
+    this.playerTwo = playerTwo;
+    this.playerOneTurn = true;
+    this.playerTwoTurn = false;
+  }
 }
